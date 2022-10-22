12ed671ff33e5a001008850677465e8da0d93ba1
Carlos Marcelo Carpes <carlosmcarpes@gmail.com>
2022-10-21 22:22:58 -0300

    dotnet

---
+++ Back/Src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11",
+                Local = "Porto Alegre",
+                Lote = "1º lote",
+                QtdePessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemUrl = "foto.png"
+            },
+            
+                new Evento(){
+                EventoId = 2,
+                Tema = "Angular 12",
+                Local = "Porto Alegre",
+                Lote = "2º lote",
+                QtdePessoas = 250,
+                DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
+                ImagemUrl = "foto1.png"
+            }
+        };
+        
+        [HttpGet]
+        public IEnumerable<Evento> Get(){
+
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id){
+
+            return _evento.Where(evento => evento.EventoId == id); //evento é uma variável criada aqui agora
+            
+        }
+    }
+}
